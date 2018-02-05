5d131cbecb44e2dcb71f35906b43719c748586f5
Melody Lee <leemeli@uw.edu>
2018-02-05 01:03:25 -0800

    modify deployment scripts

--- Server/run.sh
+++ Server/run.sh
@@ -2,11 +2,13 @@
 export TLSKEY=/tls/privkey.pem
 export TLSCERT=/tls/fullchain.pem
 
-docker rm -f gateway
+docker rm -f wwgateway
 
 docker run -d \
 -p 443:443 \
 --name wwgateway \
+-e REDISADDR=localhost:6379 \
+-e DBADDR=localhost:27017 \
 -e TLSCERT=$TLSCERT \
 -e TLSKEY=$TLSKEY \
 leemeli/wwgateway
