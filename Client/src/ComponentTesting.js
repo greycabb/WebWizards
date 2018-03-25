90e0a67ef89079f718990bb403f550570217b488
Melody Lee <leemeli@uw.edu>
2018-03-24 18:03:29 -0700

    add component testing area

---
+++ Client/src/ComponentTesting.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link, hashHistory } from 'react-router';
+
+export default class ComponentTesting extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <div>
+                This is an area to test code
+            </div>
+        );
+    }
+}
