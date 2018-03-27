4fb6bc8ade21ff2d5d75e16e5d38a3888a3b83db
Melody Lee <leemeli@uw.edu>
2018-03-27 00:49:19 -0700

    finish image library component

--- Client/src/ImageLibrary.js
+++ Client/src/ImageLibrary.js
@@ -6,9 +6,13 @@ export default class ImageLibrary extends React.Component {
         super(props);
         this.state = {
             object: {},
-            categories: []
+            categories: [],
+            viewingCategory: false,
+            currentCategory: ''
         }
         this.componentWillMount = this.componentWillMount.bind(this);
+        this.handle = this.handle.bind(this);
+        this.goBack = this.goBack.bind(this);
     }
 
     componentWillMount() {
@@ -26,18 +30,65 @@ export default class ImageLibrary extends React.Component {
             })
     }
 
-    render() {
+    handle(cat) {
+        this.setState({
+            viewingCategory: true,
+            currentCategory: cat
+        });
+    }
 
+    goBack() {
+        this.setState({
+            viewingCategory: false,
+            currentCategory:''
+        });
+    }
+
+    handleImageChoice(url) {
+        console.log(url);
+    }
+
+    render() {
         var buttons = [];
-        var categories = Object.keys(this.state.object);
-        for (var i = 0; i < categories.length; i++) {
-            var current = categories[i];
-            buttons.push(<ImageLibraryButton key={current} category={current} images={this.state.object[current]}/>);
+        var images = [];
+        if (!this.state.viewingCategory) {
+            var categories = Object.keys(this.state.object);
+            for (var i = 0; i < categories.length; i++) {
+                var current = categories[i];
+                buttons.push(<ImageLibraryButton key={current} category={current} handle={this.handle}/>);
+            }
+        }
+        if (this.state.viewingCategory) {
+            console.log('reached');
+            console.log(this.state.object[this.state.currentCategory]);
+            var imagesObj = this.state.object[this.state.currentCategory].images;
+            for (var i = 0; i < imagesObj.length; i ++) {
+                let url = imagesObj[i];
+                images.push(<img className="library-preview" src={url} key={i} onClick={() => this.handleImageChoice(url)}/>);
+            }
         }
 
         return (
             <div id="image-library-container">
-                {buttons}
+                {!this.state.viewingCategory &&
+                    <div>
+                        <h2>Images</h2>
+                        <div className="library-buttons-container">
+                            {buttons}
+                        </div>
+                    </div>
+                }
+                {this.state.viewingCategory &&
+                    <div>
+                        <div className="library-top-bar">
+                            <div id="library-back-button" className="disable-select" onClick={this.goBack}>&#x276e;</div>
+                            <h2 className="library-category-header">{this.state.currentCategory}</h2>
+                        </div>
+                        <div className="library-images-container">
+                            {images}
+                        </div>
+                    </div>
+                }
             </div>
         );
     }
@@ -51,7 +102,7 @@ class ImageLibraryButton extends React.Component {
     render() {
 
         return (
-            <button className="btn yellow-button">
+            <button className="btn library-button" onClick={() => this.props.handle(this.props.category)}>
                 {this.props.category}
             </button>
         );
