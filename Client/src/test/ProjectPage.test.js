64020af28d9be343f6be645d9681ffe1de08306f
Melody Lee <leemeli@uw.edu>
2018-05-27 15:42:14 -0700

    fix capstone project website id + build

--- Client/src/test/ProjectPage.test.js
+++ Client/src/test/ProjectPage.test.js
@@ -9,23 +9,25 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-/*describe('<DogList /> component', () => {
-  it('should render the Dog Cards', () => {
-    const wrapper = shallow(<DogList />);
-    expect(wrapper.find(DogCard).length).toEqual(5);
-  })
+//Test whether a ProjectPage renders correctly
+describe('ProjectPage /> component', () => {
 
-  it('should filter dogs on search', () => {
-    const wrapper = shallow(<DogList />);
-    wrapper.find('input').simulate('change',{target:{value:'Mix'}});
-    expect(wrapper.find(DogCard).length).toEqual(2);
+  beforeAll(() => {
+
+      global.localStorage = {
+         i2x_token: 'someToken',
+         getItem: function () {
+            return 'someToken'
+         },
+         clear: function () {
+             return ''
+         }
+      };
+   });
+
+  it('should not allow signup button to be clicked with any errors', () => {
+      const wrapper = mount(<ProjectPage />);
+      expect(wrapper.find('button').props().disabled).toEqual(true);
   });
-
-  it('should search by the entered term', () => {
-    const searchSpy = sinon.spy(DogList.prototype, 'searchDogs'); //spy for search method
-    const wrapper = shallow(<DogList />);
-    wrapper.find('input').simulate('change',{target:{value:'Mix'}});
-    expect(searchSpy.getCall(0).args[0]).toEqual('Mix');
-  })
+  
 });
-*/
