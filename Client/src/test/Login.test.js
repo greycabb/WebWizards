60b286cab37d2ecf60be36f6f0856c7282b569b9
greycabb <greycabbage@gmail.com>
2018-05-28 23:01:31 -0700

    terms and conditions page

--- Client/src/test/Login.test.js
+++ Client/src/test/Login.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-//import sinon from 'sinon';
+import sinon from 'sinon';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
